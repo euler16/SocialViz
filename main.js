47b00cbe70b2e162476958a1fd279f5c33d1914b
euler16 <nilayshrivastava1729@gmail.com>
2018-12-03 02:21:51 +0530

    added load save feature

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow() {
     });
     win.loadFile('public/index.html');
     
-    //win.webContents.openDevTools();
+    win.webContents.openDevTools();
 
     win.on('closed',()=>{
         win = null;
