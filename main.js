d9cbb861c5b8d53c56eda62c04f1ef5ef081735a
euler16 <nilayshrivastava1729@gmail.com>
2018-12-02 18:50:06 +0530

    initial photon setup

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ let win;
 function createWindow() {
     win = new BrowserWindow({
         width: 1000, 
-        height: 600,
-        'min-width': 400,
+        height: 800,
+        'min-width': 600,
         'accept-first-mouse': true,
         'title-bar-style': 'hidden'
     });
     win.loadFile('public/index.html');
     
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 
     win.on('closed',()=>{
         win = null;
